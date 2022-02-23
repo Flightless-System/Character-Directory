321fd99d0762e075df0fb57a369bd62f932fbee2
Oliver-Weiss <writingxroyalty@gmail.com>
2022-02-22 20:22:34 -0500

    server.py change

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
+from email.mime import application
 from flask_app import app
 from flask_app.controllers import users, characters
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
