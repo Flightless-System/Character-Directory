d73d0118fb54bf8cf88247f4703cb09634a2fd55
Oliver-Weiss <writingxroyalty@gmail.com>
2022-03-07 07:39:01 -0500

    enabled debugging when app runs

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
+from distutils.log import debug
 from email.mime import application
 from flask_app import app
 from flask_app.controllers import users, characters
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
