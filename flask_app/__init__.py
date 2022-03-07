cb071e4dc664634c29bae7a66fdb18be90049496
Oliver-Weiss <writingxroyalty@gmail.com>
2022-03-06 23:55:21 -0500

    Added logger code

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,4 +1,13 @@
 from flask import Flask, session
 import os
+import logging
+
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
+
+logging.basicConfig(filename = 'record.log', level = logging.DEBUG, format = f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+@app.route('/blogs')
+def blog():
+    app.logger.info('Info level log')
+    app.logger.warning('Warning level log')
+    return f"Welcome to the Blog" 
